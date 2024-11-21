32a1d24a18cb799dc74c222df5c6dc97189e958a
Sergio Bonillo <serbonlag@alu.edu.gva.com>
2024-11-21 20:27:48 +0100

    Commit

---
+++ src/Prueba.java
@@ -0,0 +1,13 @@
+public class Prueba{
+
+public static void main(String[] args) {
+
+        int x = 5;
+        int y = 6;
+        int suma= x+y;
+
+    System.out.print("La suma de "+x+" y "+y+" es = "+suma);
+
+    }
+
+}
