38168e8df4a490c4ca96bde66690bf11a5d02530
Matius365 <macius365@gmail.com>
2024-11-23 14:40:28 +0100

    prueba de cambios

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Prueba{
 
 public static void main(String[] args) {
@@ -7,7 +9,18 @@ public static void main(String[] args) {
         int suma= x+y;
 
     System.out.print("La suma de "+x+" y "+y+" es = "+suma);
+    System.out.println();
+    System.out.println();
 
+
+    int a,b,sumar;
+    Scanner sc = new Scanner(System.in);
+    System.out.print ("Introduce valor de a: ");
+    a = sc.nextInt();
+    System.out.print ("Introduce valor de b: ");
+    b = sc.nextInt();
+    sumar = a+b;
+    System.out.print ("El resultado de la suma es: " + sumar);
     }
 
 }
